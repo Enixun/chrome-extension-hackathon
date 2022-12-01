e7e5a99b3ac05a00e414da0424942af78a6c0823
Christian Ashley <ashleycz1993@gmail.com>
2022-11-30 19:22:38 -0500

    Basic block Youtube extension

---
+++ main.js
@@ -0,0 +1,4 @@
+const pageManager = $('#contents');
+const pageManagerParent = pageManager.parent();
+pageManager.remove();
+pageManagerParent.prepend('<img src="https://searchzen.org/public/code_all_teh_things.jpg">');
